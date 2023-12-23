2a216d9bae0abb6947508b858a90267e925f8434
Alperen Çıtak <citak_alperen@outlook.com>
2023-12-23 23:49:45 +0300

    DictionaryAndTupleLess

---
+++ DictionaryAndTupleLess.py
@@ -0,0 +1,34 @@
+ogrenciler = {
+    231152:{
+        "Ad-Soyad":"Ahmet Güven",
+        "Bolum":"Makine Mühendisliği",
+        "Sınıf":"2.Sınıf",
+        "Durum":"Aktif Öğrenci"
+    },
+    215473:{
+        "Ad-Soyad":"Cemil Yılmaz",
+        "Bolum":"İktisat",
+        "Sınıf":"4.Sınıf",
+        "Durum":"Aktif Öğrenci"
+    },
+    228796:{
+        "Ad-Soyad":"Fatma Camcı",
+        "Bolum":"Tıp Fakültesi",
+        "Sınıf":"3.Sınıf",
+        "Durum":"Aktif Öğrenci"
+    }
+}
+# numara = input("Sorgulamak istediğiniz öğrenci numarasını giriniz : ")
+# numara = int(numara)
+#
+# print(f"{numara} Numaralı öğrenci -->  {ogrenciler[numara]}")
+
+tuple_liste = ("araba","ev","bahçe")
+
+print(tuple_liste.index("araba"))
+print(tuple_liste[0])
+# tuple_liste[0] = "ev" //yapılamaz!!!
+
+tuple_liste = ("merhaba","naber")
+
+print(tuple_liste)
